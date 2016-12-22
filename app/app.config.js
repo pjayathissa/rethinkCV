6c3813ca9954c1a353dd353a06c235b6c71a155b
Prageeth Jayathissa <p.jayathissa@gmail.com>
2016-12-23 11:32:40 +1300

    first commit

---
+++ app/app.config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.
+  module('phonecatApp').
+  config(['$locationProvider' ,'$routeProvider',
+    function config($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider.
+        when('/phones', {
+          template: '<phone-list></phone-list>'
+        }).
+        when('/standard-list', {
+          template: '<standard-list></standard-list>'
+        }).
+        when('/phones/:phoneId', {
+          template: '<phone-detail></phone-detail>'
+        }).
+        otherwise('/phones');
+    }
+  ]);
---
+++ app/standard-list/standard-list.component.js
@@ -0,0 +1,72 @@
+'use strict';
+
+// Register `phoneList` component, along with its associated controller and template
+angular.
+  module('standardList').
+  component('standardList', {
+    templateUrl: 'standard-list/standard-list.template.html',
+      controller: ['$http', function PhoneListController($http) {
+      var self = this;
+      //set default order property for drop down list
+      this.orderProp = '-importance';
+
+
+      $http.get('experience/experience.json').then(function(response) {
+      self.experiences = response.data;
+      self.newList=self.experiences;
+      //aquire a list of skills from experience.json
+      var skills=response.data.map(function(a) {return a.skills;});
+      var skillList=[];
+      //make an array for every skill 
+      for (var ii in skills) {
+        for (var jj in skills[ii]){
+          var skill=skills[ii][jj];
+          if (skillList.indexOf(skill) === -1) {
+            skillList.push(skill);
+          };
+
+        };
+      };
+      self.skillList = skillList;
+
+
+      //For checkbox boolean control
+      //initialise skill properites selected as false
+      var skillProp={};
+      skillList.forEach(function(val, i){
+        skillProp[val]=false;
+      });
+      self.skillProp=skillProp;
+      });
+      //run function when checkbox is ticked
+
+      self.fooChanged = function(){
+        //initialise skills filter for filtering CV list
+        var skillFilter=[];
+        //determine if something was selected
+        var somethingSelected=_.contains(self.skillProp, true);
+        if (somethingSelected === true) {
+          //if selected, create a new array with the skills that were selected
+          //_.each(self.skillProp, function(val,key){if (val){skillFilter.concat(key);} })
+          _.each(self.skillProp, function(val,key){if (val){skillFilter.push(key);} })
+          console.log('something Selected');
+        } else {
+          console.log('none selected');
+          skillFilter=[];
+        };
+        var newList=[];
+        for ( var ii in self.experiences) {
+          //create a manual filter here to reset self.experiences
+          var skillDiff=_.difference(self.experiences[ii].skills, skillFilter);
+          console.log(skillDiff);
+          if (skillDiff.length === self.experiences[ii].skills.length - skillFilter.length) {
+            newList.push(self.experiences[ii]);
+            console.log('delete');
+          }
+        };
+        self.skillFilter=skillFilter.join(" ");
+        self.newList=newList;
+      };
+
+      }]
+  });
